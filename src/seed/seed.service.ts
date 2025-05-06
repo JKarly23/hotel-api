1a00066f3b54f913b5d8ac8ce2870ae843fdd807
JKarly23 <javierc.ps16@gmail.com>
2025-05-05 23:48:36 -0400

    refactor(seed): inject RoomRepository to clear rooms before seeding
    This change introduces the `RoomRepository` dependency to the `SeedService` to ensure that existing rooms are deleted before seeding new ones. This prevents duplicate entries and ensures a clean state for the seed operation.

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { RoomService } from 'src/room/room.service';
 import { roomsData } from './data/rooms.data';
 import { Room } from 'src/room/entities/room.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly roomService: RoomService) {}
+  constructor(
+    private readonly roomService: RoomService,
+    @InjectRepository(Room)
+    private readonly roomRepository: Repository<Room>,
+  ) {}
   async runSeedRooms() {
+    await this.roomRepository.delete({});
     try {
       const rooms = await Promise.all(
         roomsData.map(async (room) => this.roomService.create(room)),

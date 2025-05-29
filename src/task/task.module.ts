357241cf3334f0bcb531477f24ff0d19dc690ab5
JKarly23 <javierc.ps16@gmail.com>
2025-05-29 02:55:30 -0400

    fix(task): import ScheduleModule and initialize it in TaskModule

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BookingTask } from './task.service';
 import { BookingModule } from 'src/booking/booking.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
     providers: [
         BookingTask,
     ],
     imports: [
+        ScheduleModule.forRoot(),
         BookingModule
     ],
 })
